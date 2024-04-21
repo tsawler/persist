7fef192a8d314fb1dae58be1865a05309d7e4448
Trevor Sawler <trevor.sawler@gmail.com>
2024-04-21 09:39:50 -0300

    clean up

--- setup_test.go
+++ setup_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-var pgConnString = "host=localhost port=5433 user=postgres password=password dbname=foo sslmode=disable timezone=UTC connect_timeout=5"
+var pgConnString = "host=localhost port=5433 user=postgres password=password dbname=foo sslmode=disable"
 var mariadbConnString = "mariadb:password@tcp(localhost:3307)/foo"
 
 func TestMain(m *testing.M) {
